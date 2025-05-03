bdd07220290e7b4fe22e9908a4bead33f1e607a2
Pierpaolo Calanna <p.calanna@gmail.com>
2025-05-03 21:32:39 +0200

    Update abgrid_utils.py

--- lib/abgrid_utils.py
+++ lib/abgrid_utils.py
@@ -1,14 +1,5 @@
-"""
-Filename: abgrid_utils.py
-Description: Implements a decorator to print notifications for function execution.
-
-Author: Pierpaolo Calanna
-Date Created: May 3, 2025
-
-The code is part of the AB-Grid project and is licensed under the MIT License.
-"""
-
 from typing import Callable, Any
+import traceback
 
 def notify_decorator(argument: str) -> Callable:
     def decorator(function: Callable) -> Callable:
@@ -26,15 +17,18 @@ def notify_decorator(argument: str) -> Callable:
                 Any: The result of the original function execution.
 
             Exceptions:
-                Prints an error message if the original function raises an exception.
+                Prints an error message if the original function raises an exception,
+                along with the traceback details.
             """
             print(f"Operation '{argument}' is being currently executed.")
             try:
                 result = function(*args, **kwargs)  # Call the original function
-                print(f"Operation '{argument}' was succesfully executed.")
+                print(f"Operation '{argument}' was successfully executed.")
                 return result
             except Exception as error:
                 print(f"Error while executing operation '{argument}'. Error details: {error}")
+                print("Traceback details:")
+                traceback.print_exc()
         
         return wrapper
     
