3255216b0f640884d21b6d3b26b6f51a2ec7c839
Pierpaolo Calanna <p.calanna@gmail.com>
2025-06-13 07:17:06 +0200

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,4 @@
+import jinja2
 import matplotlib
 
 # Customize matplotlib settings
@@ -8,3 +9,8 @@ SYMBOLS = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 A_COLOR = "#0000FF"
 B_COLOR = "#FF0000"
 CM_TO_INCHES = 1 / 2.54
+
+# Initialize Jinja2 environment with a file system loader for templates
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(["./lib/templates"])
+)
