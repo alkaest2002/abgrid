6f4d17cb5a408ed571504bae4f2bc1df24f51b17
Pierpaolo Calanna <p.calanna@gmail.com>
2025-06-13 07:07:40 +0200

    Update abgrid_main.py

--- lib/abgrid_main.py
+++ lib/abgrid_main.py
@@ -191,6 +191,8 @@ class ABGridMain:
                     "sociogram.graph_ac",
                 ],
                 keys_regex_to_omit = [
+                    r"sna\.network_[a|b]",
+                    r"sna\.adjacency_[a|b]",
                     r"sna\.micro_stats_[a|b]\..*_rank",
                     r"sna\.micro_stats_[a|b]\..*_pctile",
                     r".*_robust_z",
