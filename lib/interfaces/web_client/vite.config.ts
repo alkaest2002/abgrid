966b6e121fbbb5c7eafb17d79af41a62e7e61a38
Pierpaolo Calanna <p.calanna@gmail.com>
2025-07-02 12:15:42 +0200

    Implementing web app

--- lib/interfaces/web_client/vite.config.ts
+++ lib/interfaces/web_client/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    basicSsl({
+      /** name of certification */
+      name: 'abgrid',
+      /** custom trust domains */
+      domains: ['*.auth0.com'],
+      /** custom certification directory */
+      certDir: '.devServer/cert'
+    })
   ],
 })
