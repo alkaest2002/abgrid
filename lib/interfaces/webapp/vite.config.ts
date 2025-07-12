60e3ef7e739233a8d21aba17dfa7002586492029
Pierpaolo Calanna <p.calanna@gmail.com>
2025-07-12 15:28:12 +0200

    update

--- lib/interfaces/webapp/vite.config.ts
+++ lib/interfaces/webapp/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    basicSsl({
-      /** name of certification */
-      name: 'abgrid',
-      /** custom trust domains */
-      domains: ['*.auth0.com'],
-      /** custom certification directory */
-      certDir: '.devServer/cert'
-    })
-  ],
+    plugins: [
+        tailwindcss(),
+        basicSsl({
+            /** name of certification */
+            name: "abgrid",
+            /** custom trust domains */
+            domains: [],
+            /** custom certification directory */
+            certDir: ".devServer/cert"
+        }),
+    ],
 })
