33d606521f7b7c7c4511a02f34b81d7d767bbb04
Pierpaolo Calanna <p.calanna@gmail.com>
2025-08-08 20:39:48 +0200

    Add print statement for semaphore limits in middleware
    Added a print statement to output the values of max_concurrent_requests_for_group and max_concurrent_requests_for_report for debugging purposes in RequestProtectionMiddleware.

--- lib/interfaces/fastapi/middlewares/request.py
+++ lib/interfaces/fastapi/middlewares/request.py
@@ -66,6 +66,8 @@ class RequestProtectionMiddleware(BaseHTTPMiddleware):
         
         # Use semaphores for thread-safe concurrent request limiting
         # Semaphores automatically handle counting and are exception-safe
+        self.report_semaphore = asyncio.Semaphore(max_concurrent_requests_for_report)
+        print(max_concurrent_requests_for_group, max_concurrent_requests_for_report)
         self.group_semaphore = asyncio.Semaphore(max_concurrent_requests_for_group)
         self.report_semaphore = asyncio.Semaphore(max_concurrent_requests_for_report)
         
