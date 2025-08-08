85e956e24635d474e9d3d0da0e84f3707ef3a8ff
Pierpaolo Calanna <p.calanna@gmail.com>
2025-08-08 20:46:15 +0200

    Set fixed semaphore limits in request middleware
    Replaces dynamic semaphore initialization with fixed values (15 for group, 5 for report) in RequestProtectionMiddleware. Removes debug print statement for cleaner code.

--- lib/interfaces/fastapi/middlewares/request.py
+++ lib/interfaces/fastapi/middlewares/request.py
@@ -67,9 +67,9 @@ class RequestProtectionMiddleware(BaseHTTPMiddleware):
         # Use semaphores for thread-safe concurrent request limiting
         # Semaphores automatically handle counting and are exception-safe
         self.report_semaphore = asyncio.Semaphore(max_concurrent_requests_for_report)
-        print(max_concurrent_requests_for_group, max_concurrent_requests_for_report)
-        self.group_semaphore = asyncio.Semaphore(max_concurrent_requests_for_group)
-        self.report_semaphore = asyncio.Semaphore(max_concurrent_requests_for_report)
+        
+        self.group_semaphore = asyncio.Semaphore(15)
+        self.report_semaphore = asyncio.Semaphore(5)
         
         # Store limits for reference
         self.max_concurrent_requests_for_group = max_concurrent_requests_for_group
