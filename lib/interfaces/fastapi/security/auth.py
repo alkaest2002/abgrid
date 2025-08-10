f873ee7143d9be53cc6a42fc9fc4f38538652183
Pierpaolo Calanna <p.calanna@gmail.com>
2025-08-10 11:58:01 +0200

    Update auth.py

--- lib/interfaces/fastapi/security/auth.py
+++ lib/interfaces/fastapi/security/auth.py
@@ -3,12 +3,15 @@ Author: Pierpaolo Calanna
 
 The code is part of the AB-Grid project and is licensed under the MIT License.
 """
+# ruff: noqa: B008
 
-from typing import Optional, Any, Dict
-from fastapi import Depends, HTTPException , status
+from typing import Any
+
+from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from .jwt import AnonymousJWT
+
 
 class Auth:
     """Simple JWT token verification for anonymous users."""
@@ -19,14 +22,13 @@ class Auth:
 
     async def verify_token(
         self,
-        token: Optional[HTTPAuthorizationCredentials] = Depends(HTTPBearer(auto_error=False))
-    ) -> Dict[str, Any]:
-        """"
-        Verify JWT token.
-        
+        token: HTTPAuthorizationCredentials | None = Depends(HTTPBearer(auto_error=False))
+    ) -> dict[str, Any]:
+        """"Verify JWT token.
+
         Args:
             token: JWT token from Authorization header, automatically handled by FastAPI.
-            
+
         Returns:
             A dictionary containing the decoded token payload.
 
